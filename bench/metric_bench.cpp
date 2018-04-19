976bb1630b519c55148074bffabf78fb44e0ea06
aautushka <antoninus.liberalis@gmail.com>
2018-04-19 14:50:13 +0300

    bench flat hierarchy

--- bench/metric_bench.cpp
+++ bench/metric_bench.cpp
@@ -73,8 +73,46 @@ static void bench_trie_metrics_shallow(benchmark::State& state)
     }
 }
 
+static void bench_original_metrics_flat_hierarchy(benchmark::State& state) 
+{
+    using namespace haisu::metric;
+    monitor<int, 16> mon;
+    mon.start(1);
+    while (state.KeepRunning())
+    {
+        mon.start(2);
+        mon.stop();
+        mon.start(3);
+        mon.stop();
+        mon.start(4);
+        mon.stop();
+        mon.start(5);
+        mon.stop();
+    }
+}
+
+static void bench_trie_metrics_flat_hierarchy(benchmark::State& state) 
+{
+    namespace met = haisu::metric;
+    met::monitor2<int> mon;
+    mon.start(1);
+    while (state.KeepRunning())
+    {
+        mon.start(2);
+        mon.stop();
+        mon.start(3);
+        mon.stop();
+        mon.start(4);
+        mon.stop();
+        mon.start(5);
+        mon.stop();
+    }
+}
+
 
 BENCHMARK(bench_original_metrics);
 BENCHMARK(bench_trie_metrics);
 BENCHMARK(bench_original_metrics_shallow);
 BENCHMARK(bench_trie_metrics_shallow);
+BENCHMARK(bench_original_metrics_flat_hierarchy);
+BENCHMARK(bench_trie_metrics_flat_hierarchy);
